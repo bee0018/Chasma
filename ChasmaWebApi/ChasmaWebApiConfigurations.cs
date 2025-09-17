715e10a8568671ba43564bafd3a7859b486135c3
Bryce Ellis <91974239+bee0018@users.noreply.github.com>
2025-09-17 18:40:19 -0500

    Chasma-7: Added functionality to have ChasmaWebApi to read XML config file on startup

---
+++ ChasmaWebApi/ChasmaWebApiConfigurations.cs
@@ -0,0 +1,22 @@
+﻿using ChasmaWebApi.Data.Objects;
+using ChasmaWebApi.Util;
+using System.Xml.Serialization;
+
+namespace ChasmaWebApi
+{
+    [XmlRoot("configurations")]
+    public class ChasmaWebApiConfigurations : ChasmaXmlBase
+    {
+        /// <summary>
+        /// Gets the Chasma web API url.
+        /// </summary>
+        [XmlElement("webApiUrl")]
+        public required string WebApiUrl { get; set; }
+
+        /// <summary>
+        /// Gets the database configuration options.
+        /// </summary>
+        [XmlElement("databaseConfiguration")]
+        public required DatabaseConfigurations DatabaseConfigurations { get; set; }
+    }
+}
