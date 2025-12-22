05599c78bb503c164b4fc5fd059ffbffb80f0cca
Bryce Ellis <bryceellis24@gmail.com>
2025-12-21 19:41:31 -0600

    Added local storage manager to store browser cache properties

---
+++ chasma-thin-client/src/managers/LocalStorageManager.ts
@@ -0,0 +1,20 @@
+﻿/** Gets the userId from local storage. **/
+export const getUserId = () => {
+    const userIdJson = localStorage.getItem("userId");
+    if (!userIdJson) return undefined;
+    return Number(userIdJson)
+};
+
+/** Gets the username from local storage. **/
+export const getUsername = () => {
+    const userNameJson = localStorage.getItem("username");
+    if (!userNameJson) return undefined;
+    return JSON.parse(userNameJson)
+};
+
+/** Gets the userId from local storage. **/
+export const getUserEmail = () => {
+    const emailJson = localStorage.getItem("email");
+    if (!emailJson) return undefined;
+    return JSON.parse(emailJson)
+};
