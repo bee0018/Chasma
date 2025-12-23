c5b0ea514a6edd965a2b8b17507db1ea080fe972
Bryce Ellis <bryceellis24@gmail.com>
2025-12-22 22:27:11 -0600

    Added request to push changes to git repo remote branch

---
+++ ChasmaWebApi/Data/Requests/GitPushRequest.cs
@@ -0,0 +1,15 @@
+﻿using ChasmaWebApi.Util;
+
+namespace ChasmaWebApi.Data.Requests
+{
+    /// <summary>
+    /// Class representing the git function to 'git push'.
+    /// </summary>
+    public class GitPushRequest : ChasmaXmlBase
+    {
+        /// <summary>
+        /// Gets or sets the repository identifier.
+        /// </summary>
+        public string RepositoryId { get; set; }
+    }
+}
