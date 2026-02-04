86fe385ec23fcd1e032ddc18d0bda8e88f791375
Bryce Ellis <bryceellis24@gmail.com>
2026-02-03 19:54:08 -0600

    Added stashing support to the application

---
+++ ChasmaWebApi/Data/Requests/Configuration/AddStashRequest.cs
@@ -0,0 +1,31 @@
+﻿using ChasmaWebApi.Util;
+using LibGit2Sharp;
+
+namespace ChasmaWebApi.Data.Requests.Configuration
+{
+    /// <summary>
+    /// Class representing a request to add stashed changes to a git repository.
+    /// </summary>
+    public class AddStashRequest : ChasmaXmlBase
+    {
+        /// <summary>
+        /// Gets or sets the repository identifier where the stash operation will be performed.
+        /// </summary>
+        public string RepositoryId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the user identifier associated with the stash operation.
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Gets or sets the stash modifiers to control the stashing behavior.
+        /// </summary>
+        StashModifiers StashModifier { get; set; }
+
+        /// <summary>
+        /// Gets or sets the message associated with the stashed changes.
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
