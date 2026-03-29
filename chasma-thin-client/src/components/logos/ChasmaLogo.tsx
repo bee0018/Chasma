723eabb95776f38ff258bdfa2587eaad57e949e6
BELLISPC\bryce <bryceellis24@gmail.com>
2026-03-29 17:26:21 -0500

    Updated branding and icons within the application

--- chasma-thin-client/src/components/logos/ChasmaLogo.tsx
+++ chasma-thin-client/src/components/logos/ChasmaLogo.tsx
@@ -1,26 +1,16 @@
-// ChasmaLogo.jsx
-import React from 'react';
 
-const ChasmaLogo = ({ size = 96, circleFill = "#00bfff", textFill = "#ffffff" }) => (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 48 48"
+/**
+ * Initializes a new instance of the Chasma Logo
+ * @param size The default size of the icon.
+ */
+const ChasmaLogo = ({ size = 96 }) => (
+   <img
+        src="/favicon.svg"
         width={size}
         height={size}
-    >
-        <circle cx="24" cy="24" r="20" fill={circleFill} />
-        <text
-            x="24"
-            y="24"
-            textAnchor="middle"
-            dominantBaseline="middle"
-            fontFamily="Segoe UI, Tahoma, Geneva, Verdana, sans-serif"
-            fontSize="10"
-            fill={textFill}
-        >
-            Chasma
-        </text>
-    </svg>
+        alt="Chasma Logo"
+        style={{ borderRadius: "50%" }}
+    />
 );
 
 export default ChasmaLogo;
