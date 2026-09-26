d5edb49c4977afa35446eaf3be8950e973beded0
Bryce Ellis <bryceellis24@gmail.com>
2026-09-25 22:04:38 -0500

    Chasma-487: Added a new combo box input to the checkout modal

---
+++ chasma-thin-client/src/components/application/ComboboxInput.tsx
@@ -0,0 +1,42 @@
+
+/** The interface defining the members of the ComboboxInput component. */
+interface IComboboxInput {
+    /** The list of items to select in combo box. */
+    itemList: string[];
+
+    /** The current value to display in the combo box input. */
+    currentValue: string;
+
+    /** The placeholder text to display when there is no entry in the combo box. */
+    placeholder: string
+
+    /** The function to invoke when there is a change. */
+    onChange: (newValue: string) => void;
+}
+
+/**
+ * Initializes a new instance of the ComboboxInput component.
+ * @param props The properties of the combo box.
+ * @constructor
+ */
+const ComboboxInput = (props: IComboboxInput) => {
+    return (
+        <>
+            <input
+                type="text"
+                list="custom-dropdown-options"
+                className="modal-input-field"
+                value={props.currentValue}
+                onChange={(e) => props.onChange(e.target.value)}
+                placeholder={props.placeholder}
+            />
+            <datalist id="custom-dropdown-options">
+                {props.itemList.map((item) => (
+                    <option key={item} value={item}>{item}</option>
+                ))}
+            </datalist>
+        </>
+    );
+};
+
+export default ComboboxInput;
